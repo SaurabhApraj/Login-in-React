a4e7f4a781b23b399535a94a13f9d91eed40a23b
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-25 16:46:58 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const [fullName, setfullName] = useState("");
-
-  const [lastName, setLastName] = useState("");
-  const [lastNamenew, setLastNameNew] = useState("");
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+  });
 
   const inputEvent = (event) => {
-    setName(event.target.value);
-  };
-  const inputEventTwo = (event) => {
-    setLastName(event.target.value);
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setFullName((preValue) => {
+      if (name === "fName") {
+        return { fname: value, lname: preValue.lname };
+      } else if (name === "lName") {
+        return { fname: preValue.fname, lname: value };
+      }
+    });
   };
 
   const onSubmits = (event) => {
     event.preventDefault();
-    setfullName(name);
-    setLastNameNew(lastName);
+    alert("Form Submitted");
   };
   return (
     <>
@@ -26,21 +30,22 @@ function App() {
         <form onSubmit={onSubmits}>
           <div>
             <h1>
-              Hello {fullName}
-              {lastNamenew}
+              Hello {fullName.fname} {fullName.lname}
             </h1>
             <input
               type="text"
               placeholder="Name"
+              name="fName"
               onChange={inputEvent}
-              value={name}
+              // value={fullName.fname}
             />
             <br />
             <input
               type="text"
               placeholder="Lastname"
-              onChange={inputEventTwo}
-              value={lastName}
+              name="lName"
+              onChange={inputEvent}
+              // value={fullName.lname}
             />
             <br />
             <button type="submit">Submit</button>
